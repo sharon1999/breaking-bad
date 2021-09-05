6b23b02b4aefa3260b7706b0362029121a7dea74
Sharon Antony <sharonantony51@gmail.com>
2021-09-05 11:26:27 +0530

    Finished Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import '../src/App.css'
+import Header from "./Components/Header";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    const response = await fetch(
+      `https://www.breakingbadapi.com/api/characters`
+    );
+    const data = await response.json();
+    setData(data);
+    console.log(data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
